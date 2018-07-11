22d04e02260815b79eccd552faaab4892237bccc
dashsoul92 <dashsoul92@gmail.com>
2018-07-10 21:33:20 -0600

    Refactored the __main__ file. Moved logic from __main__ to logic class and updated game logic to handle multiple values for cards with the rank of ace. Added logic to handle multiple rounds for the game.

--- logic.py
+++ logic.py
@@ -15,10 +15,27 @@ class Logic:
     def set_score(self):
         self.__dealer_score__ = 0
         self.__p1_score__ = 0
+        ace = []
         for card in self.deck.dealer_hand:
-            self.__dealer_score__ += card[1]
+            if card[1] == 1:
+                if self.__dealer_score__ + 11 == 21:
+                    self.__dealer_score__ += 11
+                elif self.__dealer_score__ + 11 < 21:
+                    self.__dealer_score__ += 11
+                else:
+                    self.__dealer_score__ += card[1]
+                    continue
+            else:
+                self.__dealer_score__ += card[1]
         for card in self.deck.player_one_hand:
-            self.__p1_score__ += card[1]
+            if card[1] == 1:
+                if self.__p1_score__ + 11 == 21:
+                    self.__p1_score__ += 11
+                else:
+                    self.__p1_score__ += card[1]
+                    continue
+            else:
+                self.__p1_score__ += card[1]
         return self.__dealer_score__, self.__p1_score__
 
     # Method used to evaluate the scores
@@ -34,8 +51,92 @@ class Logic:
             return self.__dealer_score__
         elif dealer == False and player == True:
             return self.__p1_score__
+        elif dealer == False and player == False and print_score == True:
+            print(f"\nThe dealer's score is: {self.__dealer_score__}")
+            print(f"The player's score is {self.__p1_score__}")
         else:
             return
+
+    # Method used to handle logic for each round of the game
+    def play_round(self, deck, logic, game_number = 0):
+        game_round = True
+        state = 0
+
+        while game_round:
+            # Game setup
+            if state == 0:
+                # The beginning of a round
+                # Create dealer and player hands
+                deck.create_starting_hands()
+                deck.show_hand(dealer=True, player=True)
+                state = 1
+            # Game controller for hitting and standing as the player
+            elif state == 1:
+                player_score = logic.check_score(player=True)
+                if player_score == 21:
+                    print(f"\nThe player has blackjack!")
+                    state = 2
+                elif player_score < 21:
+                    logic.check_score(dealer=True, player=True, print_score=True)
+                    player_response = input("Type 'h' to hit or 's' to stand: ")
+                    # Hitting
+                    if player_response == 'h':
+                        deck.draw_card(player=True)
+                    # Standing
+                    elif player_response == 's':
+                        state = 2
+                elif player_score > 21:
+                    dealer_score, player_score = logic.check_score(dealer=True, player=True, print_score=False)
+                    print(f"\nThe dealer won with a score of: {dealer_score}."
+                          f"\nThe player busted with a score of: {player_score}.")
+                    game_number += 1
+                    game_round = False
+                    return game_number
+            # Dealer logic for hitting and standing
+            # If the dealer's hand is 17 or higher then, the dealer will always stand
+            # If the dealer's hand is less than 17 then, the dealer must hit
+            elif state == 2:
+                dealer_score, player_score = logic.check_score(dealer=True, player=True)
+                if dealer_score == 17:
+                    if dealer_score > player_score:
+                        print(f"\nThe dealer won with a score of: {dealer_score}."
+                              f"\nThe player lost with a score of: {player_score}.")
+                        game_number += 1
+                        game_round = False
+                        return game_number
+                    elif dealer_score < player_score:
+                        print(f"\nThe dealer lost with a score of: {dealer_score}."
+                              f"\nThe player won with a score of: {player_score}.")
+                        game_number += 1
+                        game_round = False
+                        return game_number
+                    else:
+                        print(f"\nIt was a push. Both the player and dealer had a score of: {dealer_score}.")
+                        game_number += 1
+                        game_round = False
+                        return game_number
+                elif dealer_score < 17:
+                    deck.draw_card(dealer=True)
+                    dealer_score = logic.check_score(dealer=True)
+                    if dealer_score == 21:
+                        player_score = logic.check_score(player=True)
+                        if player_score == 21:
+                            print(f"It was a push. Both the player and dealer had a score of: {dealer_score}")
+                            game_number += 1
+                            game_round = False
+                            return game_number
+                        else:
+                            print(f"\nThe dealer won with a score of: {dealer_score}. "
+                                  f"\nThe player lost with a score of: {player_score}.")
+                            game_number += 1
+                            game_round = False
+                            return game_number
+                elif dealer_score > 21:
+                    print(f"\nThe dealer busted with a score of: {dealer_score}."
+                          f"\nThe player won with a score of: {player_score}")
+                    game_number += 1
+                    game_round = False
+                    return game_number
 
 
 # A method used to create the logic object
